79a30395c09515c326a52916d28aab3e39302be0
Roberto Lopez <roberloplo23@gmail.com>
2024-10-05 17:59:39 -0600

    formulario empleado básico

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -30,6 +30,8 @@ class EmpleadosController extends Controller
      */
     public function store(Request $request)
     {
+        $datosEmpleado=request()->all();
+        return response()->json($datosEmpleado);
         //
     }
 
