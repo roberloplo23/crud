102f823b495569b0465ffd7234a263175742dd53
Roberto Lopez <roberloplo23@gmail.com>
2024-10-05 17:27:52 -0600

    creacion Empleados mcr

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\EmpleadosController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Middleware de autenticación para proteger las rutas CRUD
+Route::middleware('auth')->group(function () {
+    // Rutas CRUD protegidas para empleados
+    Route::resource('empleados', EmpleadosController::class);
+}); // <- Aquí cerramos el grupo de rutas
 
 Auth::routes();
 
