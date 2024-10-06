9b4606105b35bba5fce277a4ca34f3dfdd9e907c
Roberto Lopez <roberloplo23@gmail.com>
2024-10-06 08:57:58 -0600

    Desactivar algunos mensajes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,15 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Middleware de autenticación para proteger las rutas CRUD
 Route::middleware('auth')->group(function () {
     // Rutas CRUD protegidas para empleados
-    Route::resource('empleados', EmpleadosController::class);
+    Route::resource('home', EmpleadosController::class);
 }); // <- Aquí cerramos el grupo de rutas
 
-Auth::routes();
+Auth::routes(['reset' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
